d0595dbad3b6ba7371ef88fa840a403134cd188f
trevorbox <trevorjbox@gmail.com>
2025-10-23 09:57:10 -0700

    update note

--- clusters/crc-canary-upgrade/rollout-istio-rev-default.sh
+++ clusters/crc-canary-upgrade/rollout-istio-rev-default.sh
@@ -6,6 +6,8 @@ set -x
 
 start=`date +%s`
 echo "Start: $(date -d @$start)"
+# note: need the istioctl cli from upstream release for the -o option
+# https://istio.io/latest/docs/ops/diagnostic-tools/istioctl/
 istioctl -i istio-system tag list -o json | jq -r '.[]  | select(.tag == "default").namespaces.[]' | 
 while IFS=$"\n" read -r namespace; do
 	echo "------------------"
